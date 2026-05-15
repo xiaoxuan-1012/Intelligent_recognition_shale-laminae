68a1dedc8a662e0f5a4472112aea8d386b1f4a57
xiaoxuan-1012 <2390408193@qq.com>
2026-05-15 11:33:45 +0800

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 # Evaluate the trained model
 from ultralytics import YOLO
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     model = YOLO(r"runs/detect/train3/weights/best.pt")
     model.predict(r"dataset\test\images", save=True)
 
